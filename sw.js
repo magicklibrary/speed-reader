539390b979818140b44d9ea00b6656d41c6c4b47
magicklibrary <sjmcdevitt2@gmail.com>
2026-01-14 23:22:54 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,15 @@
-const CACHE_NAME = 'speed-reader-v1';
-const urlsToCache = [
-  '/speed-reader/',
-  '/speed-reader/index.html',
+const CACHE = 'reader-v1';
+const FILES = [
+  './',
+  './index.html',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
